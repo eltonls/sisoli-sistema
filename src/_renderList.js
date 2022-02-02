e30625e27755be1f8c0230a0de69f5679f890a23
eltonls <rs.eltonribeiro@gmail.com>
2022-02-02 13:11:08 -0300

    finished list rendering code

---
+++ src/_renderList.js
@@ -0,0 +1,34 @@
+const createTD = (text) => {
+  const tdElement = document.createElement("td");
+  tdElement.innerText = text;
+  return tdElement;
+};
+
+const createTH = (vinculo) => {
+  const thElement = document.createElement("th");
+  thElement.innerText = vinculo;
+  thElement.setAttribute("scope", "row");
+  return thElement;
+};
+
+const createTR = (item) => {
+  const newTR = document.createElement("tr");
+  newTR.append(
+    createTH(item.vinculo),
+    createTD(item.titulo),
+    createTD(item.autor),
+    createTD(item.edicao),
+    createTD(item.ISSN),
+    createTD(item.ano),
+    createTD(item.editora)
+  );
+  return newTR;
+};
+
+export const RenderList = (list, tableElement) => {
+  list.forEach((item) => {
+    tableElement.append(createTR(item));
+  });
+};
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 // Import all plugins
 import * as bootstrap from 'bootstrap';
 
+// Import modules
+import * as render from "./_renderList";
+
+// Get elements
+const bookTableBodyElement = document.getElementById("book-table__body");
+
+const tableList = [
+	{
+		vinculo: "Aluno",
+		titulo: "Harry Potter",
+		autor: "J. K. Rowling",
+		edicao: 1,
+		ISSN: "978-8532511010",
+		ano: 1997,
+		editora: "Bloomsbury Publishing"	
+	},
+	{
+		vinculo: "Discente",
+		titulo: "O Senhor dos Anéis",
+		autor: "J. R. R. Tolkien",
+		edicao: 1,
+		ISSN: "8595084750",
+		ano: 1954,
+		editora: "Allen & Unwin"	
+	},
+	{
+		vinculo: "Técnico",
+		titulo: "O Pequeno Princípe",
+		autor: "A. de Saint-Exupéry",
+		edicao: 1,
+		ISSN: "978-0152465032",
+		ano: 1943,
+		editora: "Editions Gallimard"	
+	}
+];
+
+render.RenderList(tableList, bookTableBodyElement);
